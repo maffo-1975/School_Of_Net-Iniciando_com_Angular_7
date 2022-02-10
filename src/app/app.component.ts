104952b051590e8980b1b28ae2bfc88265bfa5e3
Marcio Cavalcante <mca19ntl@gmail.com>
2022-02-10 08:47:47 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Iniciando_com_Angular_7';
+}
